1e510387a2e897e4663191ce9d986092e8ba907e
BUSHIDO-program <76214131+KS-Japan@users.noreply.github.com>
2021-08-07 23:41:14 +0900

    Delete python_module_rust.py

--- day2/python_module_rust.py
+++
@@ -1,8 +0,0 @@
-import time
-import eratosthenes
-
-if __name__ == '__main__':
-    start = time.time()
-    print(eratosthenes.get_prime_numbers(10000000))
-    elapsed_time = time.time() - start
-    print ("elapsed_time:{:.3f}".format(elapsed_time) + "[sec]")
